16b7e7711b9047ba0501bb9a8020c2a81f931da9
GorkemCakirr <gorkmckr@gmail.com>
2024-01-22 17:54:10 +0300

    versiyonlama

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Img from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+  <header id="header">
+   <h1>Investment Calculator</h1>
+      <img src={Img} />
+  </header>
+   
+    
+  );
+}
